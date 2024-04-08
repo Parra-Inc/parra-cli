941302ac4825fdc721fe048b1140d2b0d0d469b8
Mick MacCallum <micksmaccallum@gmail.com>
2024-04-08 11:40:38 -0400

    fix launch screens

--- src/project_generator/templates.rs
+++ src/project_generator/templates.rs
@@ -1,6 +1,7 @@
 /// All the templates used in the project generator. Done like this instead of
 /// having a bunch of files in a templates directory to simplify packaging a
 /// standalone binary.
+///
 
 pub fn get_project_yaml_template() -> String {
     return r#"
@@ -18,20 +19,38 @@ targets:
     platform: iOS
     deploymentTarget: "17.0"
     sources: [{{ app.name }}]
-    info:
-      path: {{ app.name }}/Info.plist
+    entitlements:
+      path: App/App.entitlements
+      properties:
+        com.apple.developer.aps-environment: development
     settings:
       base:
+        GENERATE_INFOPLIST_FILE: YES
+        CODE_SIGN_IDENTITY: "-"
+        CODE_SIGNING_REQUIRED: NO
+        CODE_SIGNING_ALLOWED: NO
         PRODUCT_BUNDLE_IDENTIFIER: {{ app.bundle_id }}
+        DEVELOPMENT_ASSET_PATHS: "\"{{ app.name }}/Preview Content\""
+        INFOPLIST_KEY_UILaunchScreen_Generation: YES
+        INFOPLIST_KEY_UIApplicationSceneManifest_Generation: YES
+        INFOPLIST_KEY_UIApplicationSupportsIndirectInputEvents: YES
+        INFOPLIST_KEY_UISupportedInterfaceOrientations: "UIInterfaceOrientationLandscapeLeft UIInterfaceOrientationLandscapeRight UIInterfaceOrientationPortrait"
+        INFOPLIST_KEY_UISupportedInterfaceOrientations_iPad: "UIInterfaceOrientationLandscapeLeft UIInterfaceOrientationLandscapeRight UIInterfaceOrientationPortrait UIInterfaceOrientationPortraitUpsideDown"
     dependencies:
       - package: Parra
 
 settings:
-  GENERATE_INFOPLIST_FILE: NO
   CODE_SIGNING_ALLOWED: NO
-  MARKETING_VERSION: 1.0.0
   base:
-    CURRENT_PROJECT_VERSION: 1
+    SWIFT_VERSION: 5.9
+    MARKETING_VERSION: 1.0.0
+    CURRENT_PROJECT_VERSION: 1    
+  debug:
+    CODE_SIGN_IDENTITY: "-"
+    CODE_SIGNING_REQUIRED: NO
+    CODE_SIGNING_ALLOWED: NO
+  release:
+    CODE_SIGN_IDENTITY: iPhone Distribution
 
 packages:
   Parra:
@@ -43,8 +62,7 @@ packages:
 }
 
 pub fn get_app_swift_template() -> String {
-    return r#"
-//
+    return r#"//
 //  {{ app.camel_name }}App.swift
 //  {{ app.name }}
 //
@@ -62,7 +80,7 @@ final class {{ app.camel_name }}App: ParraApp<ParraAppDelegate, ParraSceneDelega
 
         configureParra(
             authProvider: .default(
-                tenantId: "{{ tenant.id }}",
+                workspaceId: "{{ tenant.id }}",
                 applicationId: "{{ app.id }}",
                 authProvider: {
                     fatalError("You must implement your own authentication provider")
@@ -74,14 +92,12 @@ final class {{ app.camel_name }}App: ParraApp<ParraAppDelegate, ParraSceneDelega
         )
     }
 }
-
 "#
     .to_string();
 }
 
 pub fn get_content_view_swift_template() -> String {
-    return r#"
-//
+    return r#"//
 //  ContentView.swift
 //  {{ app.name }}
 //
@@ -106,7 +122,50 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+"#
+    .to_string();
+}
 
+pub fn get_assets_json() -> String {
+    return r#"{
+  "info" : {
+    "author" : "xcode",
+    "version" : 1
+  }
+}"#
+    .to_string();
+}
+
+pub fn get_accent_color_json() -> String {
+    return r#"{
+  "colors" : [
+    {
+      "idiom" : "universal"
+    }
+  ],
+  "info" : {
+    "author" : "xcode",
+    "version" : 1
+  }
+}
+"#
+    .to_string();
+}
+
+pub fn get_app_icon_json() -> String {
+    return r#"{
+  "images" : [
+    {
+      "idiom" : "universal",
+      "platform" : "ios",
+      "size" : "1024x1024"
+    }
+  ],
+  "info" : {
+    "author" : "xcode",
+    "version" : 1
+  }
+}
 "#
     .to_string();
 }
